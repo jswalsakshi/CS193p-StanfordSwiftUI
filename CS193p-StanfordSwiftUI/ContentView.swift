0a49f2eec521d4b03dd9588ee8e5c232735892a3
sakshi.jaiswal <sakshi.jaiswal@actolap.com>
2020-05-24 16:35:08 +0530

    Introduction

--- CS193p-StanfordSwiftUI/ContentView.swift
+++ CS193p-StanfordSwiftUI/ContentView.swift
@@ -10,9 +10,55 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        HStack() {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: true)
+            }
+        }
+        .padding()
+        .foregroundColor(.orange)
     }
 }
+
+
+struct CardView: View {
+    var isFaceUp: Bool
+    var body: some View {
+        ZStack() {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            } else {
+                Text("👻")
+                RoundedRectangle(cornerRadius: 10.0).stroke()
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
